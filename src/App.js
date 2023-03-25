77dd78965b83befa2c0ed1338ab8e610c04a517b
rammotivaras <rkmotivaras77@gmail.com>
2023-03-25 15:58:07 +0530

    inicial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+function Cart() {
+  const [cartItems, setCartItems] = useState([]);
 
-function App() {
+  const addToCart = (item) => {
+    const newCartItems = [...cartItems];
+    const productItem = newCartItems.find((i) => i.name === item.name);
+
+    if  (productItem) {
+     productItem.quantity++;
+     productItem.price += item.price;
+    } else {
+      newCartItems.push({
+        ...item,
+        quantity: 1,
+      });
+    }
+    
+
+    setCartItems(newCartItems);
+  };
+
+  const incrementPrice = (item) => {
+    const newCartItems = [...cartItems];
+    const productItem = newCartItems.find((i) => i.name === item.name);
+
+    if ( productItem) {
+     productItem.price += item.price;
+    }
+
+    setCartItems(newCartItems);
+  };
+
+  const decrementPrice = (item) => {
+    const newCartItems = [...cartItems];
+    const productItem = newCartItems.find((i) => i.name === item.name);
+
+    if  (productItem) {
+     productItem.price -= item.price;
+
+      if ( productItem.price < 0) {
+     productItem.price = 0;
+      }
+    }
+
+    setCartItems(newCartItems);
+  };
+
+  const getTotalPrice = () => {
+    return cartItems.reduce((acc, item) => acc + item.price, 0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Shopping Cart</h2>
+      <ul >
+        {cartItems.map((item, index) => (
+          <li className="count" key={index}>
+    
+            {item.name} - Quantity: {item.quantity}, Price: {item.price}
+            <button className="btn-m1" onClick={() => incrementPrice(item)}>+</button>
+            <button className="btn-m2" onClick={() => decrementPrice(item)}>-</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <h3>Products</h3>
+        <div>
+          <img
+            src="./images/image1.jpg"
+            alt="Product 1"
+            width="100"
+            height="100"
+          />
+          <button className="btn-1" onClick={() => addToCart({ price: 1000 })}>
+            Add to Cart
+          </button>
+        </div>
+        <div>
+          <img
+            src="./images/image2.jpg"
+            alt="Product 2"
+            width="100"
+            height="100"
+          />
+          <button className="btn-2" 
+          onClick={() => addToCart({  price: 20})}>
+            Add to Cart
+          </button>
+        </div>
+        <div>
+          <img
+            src="./images/image3.jpg"
+            alt="Product 3"
+            width="100"
+            height="100"
+          />
+          <button className="btn-3" onClick={() => addToCart({price: 300})}>
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      <div className="price">
+        Total Price: {getTotalPrice()}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Cart;
